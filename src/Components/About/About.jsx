b44cb138ae69825b11ff51dc7152b2d5c0a01b28
Marcos Henrique <sena_marcos.fp@hotmail.com>
2024-07-12 12:10:26 -0300

    incrementando-sobre

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,28 +1,45 @@
 import React from "react";
 import "./About.css";
+import img_about from "../../assets/imagens/Imagem-2.jpg";
 
 class About extends React.Component {
   render() {
     return (
       <div className="my-about">
-        <div className="about-title">
-          <h1>Sobre Mim</h1>
-          <img src="" alt="" />
-        </div>
+        <h1>Sobre Mim</h1>
         <div className="about-sections">
           <div className="about-left">
-            <img src="" alt="" />
+            <img src={img_about} alt="" />
           </div>
           <div className="about-right">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim repellat consequatur unde obcaecati necessitatibus aut quam dolorem illum? Ut obcaecati fugiat illo et provident quia dolore accusantium corporis iusto aspernatur.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto tempore corrupti unde. Cumque odit explicabo quam quisquam voluptatibus aut quidem fugiat aspernatur, eos et magnam neque voluptates culpa. Numquam, minus!</p>
+            <p>
+              Sou desenvolvedor front-end dedicado a transformar ideias
+              criativas em websites funcionais e visualmente atraentes. Tenho
+              ampla experiência com tecnologias como HTML, CSS, JavaScript e
+              frameworks como React, e meu compromisso é sempre entregar
+              projetos de alta qualidade. Valorizo a escrita de código limpo e a
+              busca constante por aprimoramento em cada detalhe do meu trabalho.
+            </p>
+            <p>
+              <br />
+              Também sou estudante de Sistemas de Informação pela Universidade
+              Federal Rural do Semi-Árido.
+            </p>
+            <div className="about-experience">
+              <div className="years-projects">
+                <p className="p">
+                  11<span className="span">+</span>
+                </p>  
+                <span className="span-one">Projetos</span>
+              </div>
+              <div className="years-projects">
+                <p className="p">
+                  1,5<span className="span">+</span>
+                </p>
+                <span className="span-one">Anos de experiência</span>
+              </div>
+            </div>
           </div>
-          <div className="about-experience">
-            <div>primeira caixa</div>
-            <div>segunda caixa</div>
-            <div>terceira caixa</div>
-          </div>
-
         </div>
       </div>
     );

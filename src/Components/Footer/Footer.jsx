0733ca71cee033fdd804d32329e33bbf916d187a
Marcos Henrique <sena_marcos.fp@hotmail.com>
2024-08-04 01:10:55 -0300

    footer-implementação

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,38 +1,56 @@
 import React from "react";
 import "./Footer.css";
+import { Link } from "react-scroll";
 
 class Footer extends React.Component {
   render() {
     return (
-      <footer className="footer"> 
-        <div className="social-icons"> 
-            <a href="https://www.linkedin.com/in/marcos-henrique-gomess/"><i className='bx bxl-linkedin'></i></a>
-            <a href="https://github.com/marcoshgss"><i className='bx bxl-github'></i></a>
-            <a href="https://www.instagram.com/marcoshgss/"><i className='bx bxl-instagram-alt' ></i></a>
+      <footer className="footer">
+        <div className="social-icons">
+          <a href="https://www.linkedin.com/in/marcos-henrique-gomess/">
+            <i className="bx bxl-linkedin"></i>
+          </a>
+          <a href="https://github.com/marcoshgss">
+            <i className="bx bxl-github"></i>
+          </a>
+          <a href="https://www.instagram.com/marcoshgss/">
+            <i className="bx bxl-instagram-alt"></i>
+          </a>
         </div>
 
         <ul className="list">
-            <li>
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a href="#">Sobre mim</a>
-            </li>
-            <li>
-              <a href="#">Skill</a>
-            </li>
-            <li>
-              <a href="#">Experiência</a>
-            </li>
-            <li>
-              <a href="#">Contato</a>
-            </li>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <Link to="about" smooth={true} duration={500}>
+            Sobre mim
+          </Link>
+          <li>
+            <Link to="project" smooth={true} duration={500}>
+              Projetos
+            </Link>
+          </li>
+          <li>
+            <Link to="scroll" smooth={true} duration={500}>
+              Skills
+            </Link>{" "}
+          </li>
+          <li>
+            <Link to="experience" smooth={true} duration={500}>
+              Experiência
+            </Link>{" "}
+          </li>
+          <li>
+            <Link to="contact" smooth={true} duration={500}>
+              Contato
+            </Link>
+          </li>
         </ul>
         <p className="copyright">
-         &copy; Marcos Henrique | Todos os direitos reservados
+          &copy; Marcos Henrique | Todos os direitos reservados
         </p>
       </footer>
-    )
+    );
   }
 }
 
